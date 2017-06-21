425d0d4542fa480fcd1cd4b92913fcabe7191a02
BlackLee_home <webitlee@163.com>
2017-06-21 21:31:07 +0800

    添加了异常处理的前后台逻辑，半成品

---
+++ src/com/blacklee/controlleradvice/Exceptions.java
@@ -0,0 +1,45 @@
+package com.blacklee.controlleradvice;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.portlet.ModelAndView;
+
+@ControllerAdvice
+public class Exceptions {
+	
+	@ExceptionHandler({FileNotFoundException.class, NullPointerException.class, ClassCastException.class,ArrayIndexOutOfBoundsException.class, NoSuchMethodError.class, ArithmeticException.class, IndexOutOfBoundsException.class})
+	public ModelAndView codeException(Exception e){
+		ModelAndView view = new ModelAndView("/admin/html/exceptions.html");
+		view.addObject("cause", "代码书写错误");
+		view.addObject("detail", e.getStackTrace());
+		return view;
+	}
+	
+	@ExceptionHandler({SQLException.class})
+	public ModelAndView sqlsExcetion(Exception e){
+		ModelAndView view = new ModelAndView("/admin/html/exceptions.html");
+		view.addObject("cause", "操作数据库异常");
+		view.addObject("detail", e.getStackTrace());
+		return view;
+	}
+	
+	@ExceptionHandler({IOException.class})
+	public ModelAndView IOExceptions(Exception e){
+		ModelAndView view = new ModelAndView("/admin/html/exceptions.html");
+		view.addObject("cause", "输入输出异常");
+		view.addObject("detail", e.getStackTrace());
+		return view;
+	}
+	
+	@ExceptionHandler({ThreadDeath.class, IllegalThreadStateException.class})
+	public ModelAndView threadExceptions(Exception e){
+		ModelAndView view = new ModelAndView("/admin/html/exceptions.html");
+		view.addObject("cause", "线程异常");
+		view.addObject("detail", e.getStackTrace());
+		return view;
+	}
+}
