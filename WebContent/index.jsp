6049a21a3959ae97c3502fd8154d7afc0a1d24bb
Black Lee <webitlee@163.com>
2017-06-23 17:50:22 +0800

    配置了项目默认的首页，删除多余的文件

---
+++ WebContent/index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>BlackLee的博客 -- 专注全栈开发</title>
+</head>
+<body>
+</body>
+<script>
+	setTimeout(function(){
+		location.assign('getAll/1');
+	},0)
+</script>
+</html>
--- src/com/blacklee/controlleradvice/Exceptions.java
+++ src/com/blacklee/controlleradvice/Exceptions.java
@@ -4,7 +4,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
 
-import org.apache.log4j.Logger;
 import org.hibernate.TransactionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -33,6 +32,8 @@ public class Exceptions {
 		ModelAndView view = new ModelAndView("/admin/html/exceptions.html");
 		view.addObject("cause", "操作数据库异常");
 		view.addObject("detail", e.getStackTrace());
+		//错误日志
+		log.getLogger(e);
 		return view;
 	}
 	
@@ -41,6 +42,8 @@ public class Exceptions {
 		ModelAndView view = new ModelAndView("/admin/html/exceptions.html");
 		view.addObject("cause", "输入输出异常");
 		view.addObject("detail", e.getStackTrace());
+		//错误日志
+		log.getLogger(e);
 		return view;
 	}
 	
@@ -49,6 +52,8 @@ public class Exceptions {
 		ModelAndView view = new ModelAndView("/admin/html/exceptions.html");
 		view.addObject("cause", "线程异常");
 		view.addObject("detail", e.getStackTrace());
+		//错误日志
+		log.getLogger(e);
 		return view;
 	}
 	
@@ -57,6 +62,8 @@ public class Exceptions {
 		ModelAndView view = new ModelAndView("/admin/html/exceptions.html");
 		view.addObject("cause", "事务异常");
 		view.addObject("detail", e.getStackTrace());
+		//错误日志
+		log.getLogger(e);
 		return view;
 	}
 }
